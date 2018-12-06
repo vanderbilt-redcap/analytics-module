fb0cb2377de2f45941a70932fbaa18cdc87b5675
Mark McEver <mark@fastmail.net>
2018-12-06 20:27:56 +0000

    Added a warning when developing in a PHP version other than 5.4

--- AnalyticsExternalModule.php
+++ AnalyticsExternalModule.php
@@ -24,6 +24,14 @@ class AnalyticsExternalModule extends \ExternalModules\AbstractExternalModule{
 		<script src="https://player.vimeo.com/api/player.js"></script>
 		<script src="<?=$this->getUrl('analytics.js')?>"></script>
 		<?php
+
+		if($_SERVER['HTTP_HOST'] === 'localhost' && PHP_MAJOR_VERSION !== 5 || PHP_MINOR_VERSION !== 4){
+			?>
+			<script>
+				alert("Please test the <?=$this->getModuleName()?> module in PHP 5.4 for STRIDE, since UMass (and maybe UAB) are currently on 5.4.")
+			</script>
+			<?php
+		}
 	}
 
 	function redcap_survey_complete($project_id, $record, $instrument){
